7cd974a38684e2292ab7555c0efb8ba0c9e847d9
mohamed bounaime <mbounaime@enseirb-matmeca.fr>
2020-06-14 02:59:14 +0200

    .

--- Rap/urls.py
+++ Rap/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Accueil', views.index, name='index'),
+    path('Rappeurs', views.rappeurs, name='rappeurs'),
+    path('Actualites', views.actualites, name='Actualites'),
+    path('Contact', views.contact, name='contact'),
 ]
--- Rap/views.py
+++ Rap/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def rappeurs(request):
+    return render(request, 'rappeurs.html')
+
+def actualites(request):
+    return render(request, 'actualites.html')
+
+def contact(request):
+    return render(request, 'contact.html')
